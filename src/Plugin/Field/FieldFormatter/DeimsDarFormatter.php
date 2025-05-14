520fd6e1dd142e7a5b4e558b47a81e861bbb24d4
stopopol <christoph.wohner@umweltbundesamt.at>
2025-05-14 13:57:33 +0200

    Update DeimsDarFormatter.php

--- src/Plugin/Field/FieldFormatter/DeimsDarFormatter.php
+++ src/Plugin/Field/FieldFormatter/DeimsDarFormatter.php
@@ -46,29 +46,30 @@ class DeimsDarFormatter extends FormatterBase {
 
 		foreach ($items as $delta => $item) {
 		  
-			$url = "https://catalogue.lter-europe.net/elter/documents?facet=elterDeimsUri%7Chttps://deims.org/" . $item->value;
+			$api_url = "https://dar.elter-ri.eu/api/search/?q=&sort=newest&page=2&size=10&metadata_siteReferences_siteID=" . $item->value;
+			$landing_page_url = "https://dar.elter-ri.eu/search/?q=&l=list&p=2&s=5&sort=newest&f=metadata_siteReferences_siteID:" . $item->value;
 			
 			try {
-				$response = \Drupal::httpClient()->get($url."&format=json", array('headers' => array('Accept' => 'text/plain')));
+				$response = \Drupal::httpClient()->get($api_url, array('headers' => array('Accept' => ' application/json')));
 				$data = (string) $response->getBody();
 				if (empty($data)) {
 					// potentially add a more meaningful error message here in case data can't be fetched from DAR
 					\Drupal::logger('deims_dar_formatter')->notice(serialize(array()));
 				}
 				else {
-                                        $data = json_decode($data, TRUE);
-                                }
+					$data = json_decode($data, TRUE);
+                }
 			}
 			catch (GuzzleException $e) {
 				if ($e->hasResponse()) {
-                                        $response = $e->getResponse()->getBody()->getContents();
+                    $response = $e->getResponse()->getBody()->getContents();
 					\Drupal::logger('deims_dar_formatter')->notice(serialize($response));
                                 }
 				return array();
 			}
 			
-			if (intval($data["numFound"])>0) {
-				$output = "There is a total of " . $data["numFound"] . " datasets for this site available on the eLTER Digital Asset Register (DAR). <a href='" . $url . "'>Click here to get an overview of these datasets.</a><br>";
+			if (intval($data["hits"]["total"])>0) {
+				$output = "There is a total of " . $data["hits"]["total"] . " datasets for this site available on the eLTER Digital Asset Register (DAR). <a href='" . $landing_page_url . "'>Click here to get an overview of these datasets.</a><br>";
 			}
 			else {
 				// need to return empty array for Drupal to realise the field is empty without throwing an error
